8a235bb00d4032c8079b3dc4eb09f2f1e7037b09
Chinmay Mhatre <chinmaymhatre111@gmail.com>
2024-08-23 19:09:57 +0530

    feat: add Client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Cam from "./components/Camera"
+
+function App() {
+
+  return (
+    <>
+      <Cam/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Camera.tsx
@@ -0,0 +1,9 @@
+const Cam = () => {
+  return (
+    <div>
+      <img src="http://10.72.241.110:5000/video_feed" alt="Video" />
+    </div>
+  );
+};
+
+export default Cam;
